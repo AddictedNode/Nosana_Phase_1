a1b907b15eb2b23cffd84d3328e9501f00ac94a8
AddictedNode <93671524+AddictedNode@users.noreply.github.com>
2022-03-22 22:45:30 +0530

    Create First.js

---
+++ First.js
@@ -0,0 +1,12 @@
+wallet=" 1 0"
+rolls="Active rolls"
+today=`date +%Y-%m-%d.%H:%M:%S`
+
+cd /root/massa/massa-client/ && ./massa-client wallet_info; cd
+
+if [[ ${rolls[*]} =~ 0 ]]
+    then echo 'Срочно нужно купить роллы!' >>  rolls.log
+    cd /root/massa/massa-client/ && ./massa-client buy_rolls $wallet; cd
+
+else
+    echo $today 'Роллы на месте! Можно спать спокойно!' >>  rolls.log
